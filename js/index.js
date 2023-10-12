321f815bf228e3d42739f2df27470350592ee2e3
BlazerXXI <ruslandendik@gmail.com>
2023-10-12 14:23:05 +0300

    ADD: images for assorti

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 $(document).ready(() => {
+
+	// burger-menu
 	$(".burger-menu").click(() => {
 		$(".menu").toggleClass("active");
 		$(".burger-menu").toggleClass("active-burger-menu");
+		$("html").toggleClass("lock")
 	});
 });
